8c547c57c2935abd26016261d0b6bf5f18634867
Alexis Córdova <acordova@mac.com>
2017-04-05 09:09:29 -0700

    Add init Hero data from state

--- src/views/work-view/WorkView.jsx
+++ src/views/work-view/WorkView.jsx
@@ -5,6 +5,22 @@ import Hero from '../../components/hero/Hero.jsx';
 import BodyCopy from '../../components/body-copy/BodyCopy.jsx';
 
 class WorkView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hero: {}
+    };
+  }
+
+  componentWillMount() {
+    let { match } = this.props;
+
+    this.setState({
+      hero: require(`../../../data/${match.params.project}-hero.json`)
+    });
+  }
+
   render() {
     return (
       <section className="block-center content">
@@ -12,7 +28,11 @@ class WorkView extends React.Component {
           <title>Alexis Córdova | Front-End UI/UX Developer | Work</title>
         </Helmet>
 
-        <Hero />
+        <Hero
+          background={this.state.hero.background}
+          heroImage={require(`../../assets/images/work/${this.state.hero.backgroundUrl}`)}
+          alt={this.state.hero.alt}
+        />
 
         <BodyCopy />
       </section>
