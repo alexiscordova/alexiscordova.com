8f5511673ea14c8305c9f738b5192f48be6b55cd
Alexis Córdova <acordova@mac.com>
2017-04-11 16:29:53 -0700

    Rename component to CodePen

---
+++ src/components/codepen/CodePen.jsx
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+class Codepen extends Component {
+  render() {
+    return ();
+  }
+};
+
+export default Codepen;
