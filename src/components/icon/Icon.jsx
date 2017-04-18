5faff6e423921e960d0b08dfbb93c0bbcbe323f3
Alexis Córdova <acordova@mac.com>
2017-04-18 13:19:54 -0700

    Add Icon component

---
+++ src/components/icon/Icon.jsx
@@ -0,0 +1,35 @@
+import React, { PropTypes, Component } from 'react';
+
+// Component styles
+import './style.scss';
+
+class Icon extends Component {
+  render() {
+    return (
+      <a href={this.props.link}>
+        <svg
+          className={`icon ${this.props.className}`}
+          width={this.props.width}
+          height={this.props.height}
+        >
+          <use xlinkHref={`#icon-${this.props.name}`} />
+        </svg>
+      </a>
+    )
+  }
+};
+
+Icon.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  className: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  link: PropTypes.string
+};
+
+Icon.defaultProps = {
+  width: '40',
+  height: '40'
+}
+
+export default Icon;
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Avatar from '../../components/avatar/Avatar.jsx';
+import Icon from '../../components/icon/Icon.jsx';
 
+// Component styles
 import './style.scss';
 
-class Profile extends React.Component {
+let networks = [
+  {
+    "id": 1,
+    "name": "email",
+    "network": "Email",
+    "className": "icon-email",
+    "link": "mailto:alexis@alexiscordova.com"
+  },
+  {
+    "id": 2,
+    "name": "github",
+    "network": "GitHub",
+    "className": "icon-github",
+    "link": "https://github.com/alexiscordova"
+  },
+  {
+    "id": 3,
+    "name": "linkedin",
+    "network": "LinkedIn",
+    "className": "icon-linkedin",
+    "link": "https://linkedin.com/in/acordova/"
+  },
+  {
+    "id": 4,
+    "name": "codepen",
+    "network": "CodePen",
+    "className": "icon-codepen",
+    "link": "https://codepen.io/acordova/"
+  },
+  {
+    "id": 5,
+    "name": "instagram",
+    "network": "Instagram",
+    "className": "icon-instagram",
+    "link": "https://instagram.com/hellacutty"
+  },
+  {
+    "id": 6,
+    "name": "twitter",
+    "network": "Twitter",
+    "className": "icon-twitter",
+    "link": "https://twitter.com/acordova"
+  }
+];
+
+class Profile extends Component {
   render() {
     return (
       <section data-component="profile">
@@ -15,12 +62,17 @@ class Profile extends React.Component {
           <p>Front-End UI/UX Developer and Maker of Things</p>
 
           <div className="icon-container">
-            <span className="icon">Email</span>
-            <span className="icon">Github</span>
-            <span className="icon">LinkedIn</span>
-            <span className="icon">CodePen</span>
-            <span className="icon">Instagram</span>
-            <span className="icon">Twitter</span>
+            { networks.map(network => {
+              return (
+                <Icon
+                  key={network.id}
+                  className={network.className}
+                  name={network.name}
+                  network={network.network}
+                  link={network.link}
+                />
+              )
+            })}
           </div>
         </div>
       </section>
