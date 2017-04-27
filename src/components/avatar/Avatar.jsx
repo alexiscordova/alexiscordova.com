560dd74596f3d83545bbfefb424315176ab8db5d
Alexis Córdova <acordova@mac.com>
2017-04-27 11:54:11 -0700

    Move name, title to Avatar component

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -6,13 +6,17 @@ class Avatar extends Component {
     return (
       <div className="avatar">
         <img className="avatar-image" src={this.props.avatarImage} />
+        <h2>{this.props.name}</h2>
+        <p>{this.props.title}</p>
       </div>
     );
   }
 };
 
 Avatar.propTypes = {
-  avatarImage: PropTypes.string.isRequired
+  avatarImage: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default Avatar;
