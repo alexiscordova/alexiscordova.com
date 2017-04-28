0ff642f90c5851f73e57d5921783f91cd6ef901c
Alexis Córdova <acordova@mac.com>
2017-04-28 12:48:54 -0700

    Add Global Header classes

--- src/components/global-header/GlobalHeader.jsx
+++ src/components/global-header/GlobalHeader.jsx
@@ -5,12 +5,12 @@ import './style.scss';
 class GlobalHeader extends Component {
   render() {
     return (
-      <header className="global-header">
+      <header data-component="global-header">
         <div className="row container block-center">
-            <Link to="/" className="logo">
-              <img width="40" height="40" src={require('Svg/logo-outline-color.svg')} />
-              <h1>
           <div className="column-small-12 column-medium-6">
+            <Link to="/" className="logo-container">
+              <img className="logo-image" width="40" height="40" src={require('Svg/logo-outline-color.svg')} />
+              <h1 className="logo-text">
                 Alexis Córdova
                 <span className="is-hidden">Front-End Web Developer</span>
               </h1>
