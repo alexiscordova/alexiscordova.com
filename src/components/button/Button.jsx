e80bb3d892504c3da622343036cb71687df7d130
Alexis Córdova <acordova@mac.com>
2017-05-01 14:33:16 -0700

    Add classnames to Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,16 +1,27 @@
 import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
 import './style.scss';
 
 class Button extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleClick() {
     window.open(this.props.link)
   }
 
   render() {
+    let classes = this.props.classes.split(' '),
+        btnClass = classNames('btn', classes);
+
     return (
       <button
-        className={this.props.classes}
-        onClick={this.handleClick.bind(this)}
+        type="button"
+        className={btnClass}
+        onClick={this.handleClick}
       >
         {this.props.text}
       </button>
