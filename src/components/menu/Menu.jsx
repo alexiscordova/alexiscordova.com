c6c232e4f8252eb25fb7f6e9bcdff479866bbf56
Alexis Córdova <acordova@mac.com>
2017-05-01 09:44:17 -0700

    Restructure and update Menu component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import './styles.scss';
+import React, { Component, PropTypes } from 'react';
+import './style.scss';
 
-class Menu extends React.Component {
+class Menu extends Component {
   constructor(props) {
     super(props);
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+
+    this.state = {
+      isMenuOpen: false
+    };
+  }
+
+  // This sends the current menu state back up to the parent to keep both menu and icon in sync
+  // However, this is bizarre syntax and if componentDidUpdate is used, the browser freaks out
+  // https://facebook.github.io/react/docs/react-component.html#setstate
+  toggleMenu() {
+    this.setState({
+      isMenuOpen: !this.state.isMenuOpen
+    }, () => {
+      this.props.handleMenuState(this.state.isMenuOpen);
+    });
   }
 
   render() {
-    return ();
+    return (
+      <div className="navigation-trigger">
+        <div className={menuClass} onClick={this.toggleMenu}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 50 50">
+            <g fillRule="evenodd">
+              <rect className="line-1" width="50" height="3" y="36" rx="1.5" />
+              <rect className="line-2" width="50" height="3" y="18" rx="1.5" />
+              <rect className="line-3" width="50" height="3" rx="1.5" />
+            </g>
+          </svg>
+
+        </div>
+      </div>
+    );
   }
-};
+}
+
+Menu.propTypes = {
+  handleMenuState: PropTypes.func
+}
 
 export default Menu;
