4ba4883f1cdd19fedd3893614b6b87858d310ba6
Alexis Córdova <acordova@mac.com>
2017-05-02 15:03:13 -0700

    Add CodePen styles

--- src/components/codepen/CodePen.jsx
+++ src/components/codepen/CodePen.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import './style.scss';
 
 class CodePen extends Component {
   render() {
