5427be9181fba9cc7bc3567b8401cfefcd2afa5b
Alexis Córdova <acordova@mac.com>
2017-05-09 12:37:59 -0700

    Add Redux to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Routes from './router';
 import './styles/style.scss';
+import { Provider } from 'react-redux'
+import store from './store'
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <Component key={module.hot ? new Date() : undefined} />
-    </AppContainer>,
+    <Provider store={store}>
+      <AppContainer>
+        <Component key={module.hot ? new Date() : undefined} />
+      </AppContainer>
+    </Provider>,
     document.querySelector('#app')
   );
 };
