bdc499a7c4076f881c03c7c242c2486e2888a0ec
Alexis Córdova <acordova@mac.com>
2017-05-09 12:41:17 -0700

    Add Redux to Global Navigation

--- src/components/global-navigation/global-navigation.jsx
+++ src/components/global-navigation/global-navigation.jsx
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
+import { connect } from 'react-redux'
+import { addButtonAnimation, removeButtonAnimation } from '../../actions/buttonActions';
+
+@connect(store => {
+  return {
+    button: store.button
+  }
+})
 
 class GlobalNavigation extends Component {
+  handleClick() {
+    if (!this.props.button.isClassAdded) {
+      this.props.dispatch(addButtonAnimation('ripple'))
+
+      setTimeout(() => {
+        this.props.dispatch(removeButtonAnimation('ripple'))
+      }, 3000)
+    }
+  }
+
   render() {
     return (
       <nav data-component="global-navigation" className="no-gutters column-small-12 column-medium-offset-3 column-medium-3">
@@ -11,7 +29,7 @@ class GlobalNavigation extends Component {
           <li className="navigation-item" id="about">
             <NavLink to="/about" activeClassName="is-active" className="navigation-link" title="About Alexis">About</NavLink>
           </li>
-          <li className="navigation-item" id="resume-jump">
+          <li className="navigation-item" id="resume-jump" onClick={this.handleClick}>
             <a href="/#resume" className="navigation-link" title="Jump to résumé download button">Résumé</a>
           </li>
         </ul>
