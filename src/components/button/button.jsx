b0b28b2a9c75dd6864babda7b9a033a9848a3bae
Alexis Córdova <acordova@mac.com>
2017-05-09 12:42:12 -0700

    Add Redux to Button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './style.scss';
+import { connect } from 'react-redux'
+@connect(store => {
+  return {
+    button: store.button
+  }
+})
 
 class Button extends Component {
   render() {
     const classes = this.props.classes.split(' '),
-          btnClass = classNames('btn', classes);
+          animationClass = this.props.button.payload,
+          btnClass = classNames('btn', classes, {
+            [animationClass]: this.props.button.isClassAdded
+          })
 
     return (
       <button type="button" className={btnClass} onClick={this.props.handleClick}>
