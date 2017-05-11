b2123fd5bfcec8a0e4588842c7fa787a4bcc7ee9
Alexis Córdova <acordova@mac.com>
2017-05-11 12:18:44 -0700

    Setup proper deep linking for dev and prod

--- tools/server.js
+++ tools/server.js
@@ -1,55 +1,61 @@
 /* eslint-disable no-console */
-const express = require('express');
-const webpack = require('webpack');
-const path = require('path');
-const open = require('open');
-const compression = require('compression');
-const config = require('../webpack.config.dev');
-const host = 'http://localhost';
-const port = 3000;
-const app = express();
-const environment = app.get('env');
-
-let compiler = webpack(config),
-    indexFile = path.join(compiler.outputPath, 'index.html');
-
-let serverSetup = () => {
-  app.get('*', (request, response, next) => {
-    compiler.outputFileSystem.readFile(indexFile, (error, result) => {
-      if (error) return next(error);
-
-      response.set('content-type', 'text/html');
-      response.send(result);
-      response.end();
-    });
-  });
-};
+const express = require('express')
+const webpack = require('webpack')
+const path = require('path')
+const open = require('open')
+const compression = require('compression')
+const chalk = require('chalk')
+const config = require('../webpack.config.dev')
+const host = 'http://localhost'
+const port = 3000
+const app = express()
+const environment = app.get('env')
+const compiler = webpack(config)
 
 if (environment === 'development') {
+  const indexFile = path.join(compiler.outputPath, 'index.html')
+
   app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath,
     stats: {
       colors: true
     }
-  }));
+  }))
 
-  app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-hot-middleware')(compiler))
 
-  serverSetup();
+  app.get('*', (request, response, next) => {
+    compiler.outputFileSystem.readFile(indexFile, (error, result) => {
+      if (error) return next(error)
+
+      response.set('content-type', 'text/html')
+      response.send(result)
+      response.end()
+    })
+  })
+
+  compiler.plugin('invalid', filename => {
+    console.log(chalk.green('FILE CHANGED:'), chalk.blue.underline(filename))
+  })
 } else if (environment === 'production') {
-  app.use(compression());
-  app.use(express.static('dist'));
+  app.use(compression())
+  app.use(express.static('dist'))
 
-  serverSetup();
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
+  })
 } else {
-  console.log('NODE_ENV not set!');
+  console.log('NODE_ENV not set!')
 }
 
 app.listen(port, error => {
   if (error) {
-    console.log(error);
+    console.log(error)
   } else {
-    open(`${host}:${port}`);
+    console.log(chalk.green(`Listening on ${host}:${port}…`))
+    open(`${host}:${port}`)
   }
-});
+})
+
+module.exports = app
